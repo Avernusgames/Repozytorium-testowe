c8bfb1b7af372c331e960dc0a71a906d452f1aca
Filip Skrobol <filipskrobol@o2.pl>
2024-10-28 18:10:07 +0100

    ulepszenie

---
+++ Projekt 2/Assets/scripts/collectableitem controler.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableitemcontroler : MonoBehaviour
+{
+
+    public PlayerController playerController;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag =="Player")
+        {
+
+            playerController.zebranePunkty++;
+
+            Debug.Log(playerController.zebranePunkty);
+
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
+
+
+    }
+
+ 
+   
+    
+    
+}
+
+
--- Projekt 2/Assets/scripts/control player.cs
+++ Projekt 2/Assets/scripts/control player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectableItemController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     int liczbacalkowita; // np. 1,2,54,76
     float liczbaprzecinkowa = 0.1f; // np, 4.32423, 56.23423
@@ -12,19 +12,19 @@ public class CollectableItemController : MonoBehaviour
     public int zebranePunkty;
 
 
-    // Start is called before the first frame update
+    
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
 
-        //Wszystko co tutaj zamiescimy wykonuje sie podczas staru gry/aplikacji
+       
 
 
 
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (Input.GetAxis("Horizontal") != 0)
@@ -40,7 +40,7 @@ public class CollectableItemController : MonoBehaviour
             rigidbody.AddForce(0, 0, directionVertical * Time.deltaTime * speed, ForceMode.Impulse);
 
         }
-
+        
         if (transform.position.y < 0)
         {
             transform.position = startPoint.position;
