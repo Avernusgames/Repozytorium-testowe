8f14f09cd9403892c48c90e920db888ed1ffc0b7
Filip Skrobol <filipskrobol@o2.pl>
2024-10-28 18:19:36 +0100

    class

--- Projekt 2/Assets/scripts/control player.cs
+++ Projekt 2/Assets/scripts/control player.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        //rigidbody = GetComponent<Rigidbody>();
 
        
 
